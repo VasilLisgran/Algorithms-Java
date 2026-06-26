8527754e883e26ffd6f476b78cc8c9768b22d4f7
Vasilii Kalinkin <148688094+VasilLisgran@users.noreply.github.com>
2026-06-26 16:53:34 +0300

    Refactor isPrime method to use long type

--- Mathematical/Prime.java
+++ Mathematical/Prime.java
@@ -2,17 +2,14 @@ import java.util.Scanner;
 
 public class Prime {
     public static Scanner in = new Scanner(System.in);
-    static boolean isPrime(int n){
-        if(n<=1)
-            return false;
-        if (n == 2 || n == 3)
-            return true;
-        if (n%2 == 0 || n%3==0){
-            return false;
-        }
-        for (int i = 5; i < Math.sqrt(n); i+=6) {
-            if(n%i == 0 || n%(i+2) == 0)
-                return false;
+    static boolean isPrime(long n){
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+
+        // Trial Division
+        for (long i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) return false;
         }
         return true;
     }
@@ -26,5 +23,8 @@ public class Prime {
         }
     }
 }
+
+
+
 
 
